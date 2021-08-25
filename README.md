cdb57ba04a1f0df6b81cfbbd1e1249025a124c57
ukamal <ukamal@isabel.eu>
2021-08-25 12:35:13 +0200

    add JWT support.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 ### Instantiate creche db ####
 docker-compose UP
+
+### Authenticate ####
+Post: localhost:8090/creche/authenticate
+Content-Type : application/json
+Body: raw :
+{
+"username":"crecheAdmin",
+"password":"password"
+}
+
+Response: <JWT Token>
+### save Baby api ###
+Post : localhost:8090/creche/baby
+Header:
+Content-Type : application/json
+Authorization : Bearer <JWT Token returned after authentication>
---
+++ src/main/java/be/techfix/creche/controllers/JwtAuthenticationController.java
@@ -0,0 +1,51 @@
+package be.techfix.creche.controllers;
+
+import be.techfix.creche.jwt.JWTUtility;
+import be.techfix.creche.jwt.JwtRequest;
+import be.techfix.creche.jwt.JwtResponse;
+import be.techfix.creche.service.JwtUserDetailedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class JwtAuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JWTUtility jwtTokenUtil;
+
+    @Autowired
+    private JwtUserDetailedService userDetailsService;
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+}
